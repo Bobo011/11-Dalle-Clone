55e701ad30408e449b7c8e3ca8de8d8594215ed4
George <alximik15@gmail.com>
2023-10-02 19:34:15 +0400

    Trying to fix Fetch Error

--- clinet/src/pages/CreatePost.jsx
+++ clinet/src/pages/CreatePost.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import { preview } from "../assets";
@@ -15,50 +15,73 @@ const CreatePost = () => {
   });
 
   const [generatingImg, setGeneratingImg] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-  const [loading, setloading] = useState(false);
+  const handleChange = (e) =>
+    setForm({ ...form, [e.target.name]: e.target.value });
 
- const generateImage = async () => {
-  if (form.prompt) {
-    try {
-      setGeneratingImg(true);
-      const response = await fetch(
-        "https://dalle-arbb.onrender.com/api/v1/dalle",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            prompt: form.prompt,
-          }),
-        }
-      );
-
-      const data = await response.json();
-      setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}` });
-    } catch (err) {
-      alert(err);
-    } finally {
-      setGeneratingImg(false);
-    }
-  } else {
-    alert("Please provide proper prompt");
-  }
-};
-
-  
-  const handleSubmit = () => {};
-  
-  
-  const handleChange = (e) => {
-   setForm({...form, [e.target.name] : e.target.value})
+  const handleSurpriseMe = () => {
+    const randomPrompt = getRandomPrompt(form.prompt);
+    setForm({ ...form, prompt: randomPrompt });
   };
 
-  const handleSurpriseMe = (e) => {
-    const randomPrompt = getRandomPrompt(form.prompt)
+  const generateImage = async () => {
+    if (form.prompt) {
+      try {
+        setGeneratingImg(true);
+        const response = await fetch(
+          "https://dalle-arbb.onrender.com/api/v1/dalle",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              prompt: form.prompt,
+            }),
+          }
+        );
 
-    setForm({...form, prompt:randomPrompt})
+        const data = await response.json();
+        setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}` });
+      } catch (err) {
+        alert(err);
+      } finally {
+        setGeneratingImg(false);
+      }
+    } else {
+      alert("Please provide proper prompt");
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (form.prompt && form.photo) {
+      setLoading(true);
+      try {
+        const response = await fetch(
+          "https://dalle-arbb.onrender.com/api/v1/post",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ ...form }),
+          }
+        );
+
+        await response.json();
+        alert("Success");
+        navigate("/");
+      } catch (err) {
+        alert(err);
+      } finally {
+        setLoading(false);
+      }
+    } else {
+      alert("Please generate an image with proper details");
+    }
   };
 
   return (
@@ -66,30 +89,33 @@ const CreatePost = () => {
       <div>
         <h1 className="font-extrabold text-[#222328] text-[32px]">Create</h1>
         <p className="mt-2 text-[#666e75] text-[14px] max-w-[500px]">
-          Create a collection of imaginative and visually stunning images
-          through by DALL-E AI and share them with the community
+          Generate an imaginative image through DALL-E AI and share it with the
+          community
         </p>
       </div>
+
       <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
         <div className="flex flex-col gap-5">
           <FormField
-            labelName="Your name"
+            labelName="Your Name"
             type="text"
             name="name"
-            placeholder="John Doe"
+            placeholder="Ex., john doe"
             value={form.name}
             handleChange={handleChange}
           />
+
           <FormField
             labelName="Prompt"
             type="text"
             name="prompt"
-            placeholder="an armchair in the shape of an avocado"
+            placeholder="An Impressionist oil painting of sunflowers in a purple vase…"
             value={form.prompt}
             handleChange={handleChange}
             isSurpriseMe
             handleSurpriseMe={handleSurpriseMe}
           />
+
           <div className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center">
             {form.photo ? (
               <img
@@ -104,6 +130,7 @@ const CreatePost = () => {
                 className="w-9/12 h-9/12 object-contain opacity-40"
               />
             )}
+
             {generatingImg && (
               <div className="absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg">
                 <Loader />
@@ -121,6 +148,7 @@ const CreatePost = () => {
             {generatingImg ? "Generating..." : "Generate"}
           </button>
         </div>
+
         <div className="mt-10">
           <p className="mt-2 text-[#666e75] text-[14px]">
             ** Once you have created the image you want, you can share it with
--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -1,43 +1,39 @@
-import express from "express";
-import * as dotenv from "dotenv";
-import { Configuration, OpenAIApi } from "openai";
+import express from 'express';
+import * as dotenv from 'dotenv';
+import { Configuration, OpenAIApi } from 'openai';
 
 dotenv.config();
 
 const router = express.Router();
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.OPEN_AI_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
 
-router.route("/").get((req, res) => {
-  res.send("Hello From DALL-E!");
+router.route('/').get((req, res) => {
+  res.send('Hello From DALL-E!');
 });
 
-
-router.route('/').post(async (req,res)=>{
-
+router.route('/').post(async (req, res) => {
   try {
-    const {prompt} = req.body
+    const { prompt } = req.body;
 
     const aiResponse = await openai.createImage({
       prompt,
       n: 1,
-      size:'1024x1024',
-      response_format:'b64_json'
-    })
+      size: '1024x1024',
+      response_format: 'b64_json',
+    });
 
-const image = aiResponse.data.data[0].b64_json;
+    const image = aiResponse.data.data[0].b64_json;
 
-
-res.status(200).json({photo:image});
+    res.status(200).json({ photo: image });
   } catch (error) {
     console.log(error);
-    res.status(500).send(error?.response.data.error.message)
+    res.status(500).send(error?.response.data.error.message);
   }
-})
-
+});
 
 export default router;
