b8091f4ae0bdfd3d0c412c14634f58c1c7a3ceb7
George <alximik15@gmail.com>
2023-10-02 15:58:54 +0400

    Routes Testing

--- server/index.js
+++ server/index.js
@@ -2,19 +2,24 @@ import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
 
-import connectDB from './mongodb/connect.js';
+import connectDB from "./mongodb/connect.js";
+import postRoutes from "./routes/postRoutes.js";
+import dalleRoutes from "./routes/dalleRoutes.js";
 
 dotenv.config();
 
 const app = express();
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
 
-app.use(cors())
-app.use(express.json({limit : '50mb'}));
+app.use("/api/v1/post", postRoutes);
+app.use("/api/v1/dalle", dalleRoutes);
 
 app.get("/", async (req, res) => {
- 
-   res.send('Hello from DALL-E!')
+  res.status(200).json({
+    message: "Hello from DALL.E!",
   });
+});
 
 const startServer = async () => {
   try {
@@ -25,4 +30,4 @@ const startServer = async () => {
   }
 };
 
-startServer()
+startServer();
